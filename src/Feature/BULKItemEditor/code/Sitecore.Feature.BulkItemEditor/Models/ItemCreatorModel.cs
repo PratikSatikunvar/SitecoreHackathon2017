f822790b0b0c9c9cb91cca157aabce69843e50a8
vtalegaonkar <vrushal.hi@gmail.com>
2017-03-04 10:38:08 +0530

    Updated BulkItemCreator Project

---
+++ src/Feature/BULKItemEditor/code/Sitecore.Feature.BulkItemEditor/Models/ItemCreatorModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.BulkItemEditor.Models
+{
+    public class ItemCreatorModel
+    {
+        public string ParentNode { get; set; }
+        public string TemplateId { get; set; }
+        public int NumberOfItems { get; set; }
+        public List<string> Languages { get; set; }
+    }
+}
--- src/Feature/BULKItemEditor/code/Sitecore.Feature.BulkItemEditor/Properties/AssemblyInfo.cs
+++ src/Feature/BULKItemEditor/code/Sitecore.Feature.BulkItemEditor/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d3bbdf53-11f0-4d1f-b236-f32498b17a88")]
+[assembly: Guid("5e786890-5a05-489b-872b-904b048e28d6")]
 
 // Version information for an assembly consists of the following four values:
 //
